26f017d2a63ce1463ac365033835c74ccb5499a7
kylesaldana5 <kylesaldana5@gmail.com>
2017-12-05 08:47:23 -0600

    added a count of each number of grade

--- grades.js
+++ grades.js
@@ -4,6 +4,39 @@ var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
     // What is the lowest grade ?
         // What is the highest grade ?
 
+// Loop over an array of student grades(values from 50 - 100) and output how many of each grades there are.
+
+// A score of 50 - 60 is an F
+// A score of 61 - 70 is a D
+// A score of 71 - 80 is a C
+// A score of 81 - 90 is a B
+// A score of 91 - 100 is an A
+
+gradeArry = [];
+
+
+for (let i = 0; i < scores.length; i++) {
+    let score = scores[i];
+    if (score > 91) {
+        gradeArry.push(`${score} A`)
+    }
+    else if (score > 81) {
+        gradeArry.push(`${score} B`)
+    }
+    else if (score > 71) {
+        gradeArry.push(`${score} C`)
+    }
+    else if (score > 61) {
+        gradeArry.push(`${score} D`)
+    }
+    else if (score > 51) {
+        gradeArry.push(`${score} F`)
+    } 
+    
+}
+
+console.log(gradeArry.sort())
+
 
 let lowGrade = Math.min.apply(null, scores);
 let highGrade = Math.max.apply(null, scores);
